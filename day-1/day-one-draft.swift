a27fdaa48951b9effac0ca551bd2463908013574
jrectenwald <jimrec101@gmail.com>
2016-01-09 13:00:51 -0500

    first commit

---
+++ day-1/day-one-draft.swift
@@ -0,0 +1,35 @@
+/** 
+General Goal: 
+Students will create their first swift program
+Students will learn about swift data types, constants and variables, and operations
+Students will learn to develop programs use TDD
+Students will complete an iPhone application
+
+
+SWBATs
+download Xcode
+understand the purpose of Xcode and the idea of a compiler
+understand the difference between Int, Double, String, and Boolean data types 
+understand the difference between using a constant and a variable
+demonstrate type annotation, understand it is unneccesary because of type inference
+follow convention for naming variables
+print data to the Xcode console
+understand what a code comment is and how to write them
+
+
+
+
+
+
+LabIdeas
+-Playlist App: hard-code a top ten list of songs and format it so that it looks cool. Use a different variable for each song.
+        -second level iteration: use array for songs, dictionary for songs and artists
+        -third level iteration: include logic in the code for... something
+
+-Bio App: Make a bio about your partner. Begins with a skeleton app, students need to fill in constants, variables, and arrays. Have a place in the file structure to use a picture? 
+
+
+-Calculator App: 
+
+*/
+
