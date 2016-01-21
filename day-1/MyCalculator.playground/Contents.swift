91577abf0e635edbe6646766680185338cb8bee7
jrectenwald <jimrec101@gmail.com>
2016-01-21 16:10:17 -0500

    add calculator lab and example

---
+++ day-1/MyCalculator.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+var answer: Double
+
+
+
+var mathProblem = "Welcome to Upperline Calculator! First, change the value of this message to a math problem, like 1 + 1 = . Then, store the answer to the math problem in the answer variable. Finally, print out the math problem with the answer concatenated in at the end. Run the program by hitting the blue triangle in the bottom-right and test your result!"
---
+++ day-1/MyPlaygroundExample.playground/Contents.swift
@@ -0,0 +1,7 @@
+var answer: Double
+
+answer = 3.0 / 1.5 + 17 * 2
+
+var mathProblem = "3.0 / 1.5 + 17 * 2 = "
+
+print(mathProblem + String(answer))
