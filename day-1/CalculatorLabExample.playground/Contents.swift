7ebf450d3382127820a402e271f1f7a5a0217434
jrectenwald <jimrec101@gmail.com>
2016-01-26 12:38:35 -0500

    added calculator example code

--- day-1/CalculatorLabExample.playground/Contents.swift
+++ day-1/CalculatorLabExample.playground/Contents.swift
@@ -26,10 +26,10 @@ num1 = 55
 
 num2 = 40
 
-answer
+answer = 55 - 40
 
 //interpolate so that it prints the string "55 - 40 = 15"
-print("")
+print("\(num1) + \(num2) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
@@ -39,68 +39,68 @@ num1 = 4
 
 num2 = 3
 
-answer
+answer = num1 * num2
 
 //interpolate so that it prints the string "4 * 3 = 12"
-print("")
+print("\(num1) x \(num2) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
 // 3
 // division
-num1
+num1 = 20
 
-num2
+num2 = 4
 
-answer
+answer = 20 / 4
 
 //interpolate so that it prints the string "20 / 4 = 5"
-print("")
+print("\(num1) / \(num2) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
 // 4
 // modulus
 // use the modulus (%) operator to find the remainder when you divide 453 / 7
-num1
+num1 = 453
 
-num2
+num2 = 7
 
-answer
+answer = 453 % 7
 
 //interpolate so that it prints the string, complete with the answer "453 % 7 = ___"
-print("")
+print("\(num1) % \(num2) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
 // 5
 // exponents
 // use a method in the Darwin library to find the answer to 54^3
-num1
+num1 = 54
 
-num2
+num2 = 3
 
-answer
+var power = pow(54.0, 3.0)
 
 //interpolate so that it prints the string "54^3 = ___"
-print("")
+print("\(num1)^\(num2) = \(power)")
 
 
 //-------------------------------------------------------------------------------------------------
 // 6
 // mixed bag
-num1
+num1 = 60
 
-num2
+num2 = 5
 
-var num3: Int
+var num3: Int = 8
 
-var num4: Int
+var num4: Int = 3
 
-answer
+answer = (num1 / num2) + (num3 % num4)
 
 //interpolate so that it prints the string "(60 / 5) + (8 % 3)"
-print("")
+print("(\(num1) / \(num2)) + (\(num3) % \(num4)) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
@@ -108,12 +108,8 @@ print("")
 // mathematical constants
 // use the Darwin library to get the value of pi
 
-//var pi = ___
+var pi = M_PI
 
-answer
 
 //interpolate so that it prints the string "The value of pi is approximately 3.141592653589793"
-print("")
-
-
-
+print(pi)
