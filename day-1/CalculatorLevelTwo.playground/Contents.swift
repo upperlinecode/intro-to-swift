ac1cdcab34efbbb3aac8f756b3228abf8711a982
jrectenwald <jimrec101@gmail.com>
2016-01-31 20:00:57 -0500

    added calculator level two lab

---
+++ day-1/CalculatorLevelTwo.playground/Contents.swift
@@ -0,0 +1,63 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Darwin
+
+
+var message = "This calculator will be a little bit more advanced than the last one we made. We are going to make our program a little bit smarter by adding conditionals."
+
+//Level One
+
+var num1: Double // = add some number here
+
+var num2: Double // = add some other number here
+
+var mathOperator: String // = put a +, -, *, or / here
+
+var answer: Double
+
+//add conditional statement here
+
+
+
+print(answer)
+
+
+
+
+//Level 2
+
+num1 // = add some number here
+
+num2 // = add some other number here
+
+mathOperator // = put a +, -, *, or / here
+
+answer
+
+var isRounded: Bool // = add true if you want to round any decimal off the end of your answer, and put false otherwise
+
+//add conditional statement here
+
+
+
+print(answer)
+
+
+//Level 3
+//Add more math functions to your calculator. Start out with a power function and a square root function, and then move on to some more advanced tools like sine or log.
+
+num1 // =
+
+num2 // =
+
+mathOperator // =
+
+answer // =
+
+isRounded // =
+
+//add conditional statement here
+
+
+
