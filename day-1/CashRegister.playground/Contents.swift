75a8754c085471877230d2536bc23791d924492f
jrectenwald <jimrec101@gmail.com>
2016-02-01 10:40:47 -0500

    add extra task to cash register lab

---
+++ day-1/CashRegister.playground/Contents.swift
@@ -0,0 +1,83 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var message = "In order to help out in the checkout line, we're going to write a program to calculate the price of apples at a store. A purchase of less than 5 apples costs 50 cents per apple. A purchase of between 6 and 24 apples costs 40 cents per apple. Any purchase of more than 25 apples costs a flat fee of $10.00"
+
+
+var pricePerApple: Double // = ___
+
+var numberOfItems: Int // = ___
+
+var totalCost: Double // = ___
+
+//write conditional statement here to calculate the correct total cost
+
+
+
+
+
+
+
+//print a message to the customer telling them the total cost of their purchase
+message // = ___
+print(message)
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------
+//Add an additional branch to the conditional statement that removes 10% of the cost if
+
+var onSale: Bool // = ___
+
+pricePerApple // = ___
+numberOfItems // = ___
+totalCost // = ___
+
+
+//write your new conditional statement here
+
+
+
+//print a message to the customer telling them the total cost of their purchase
+message // = ___
+print(message)
+
+
+
+//-------------------------------------------------------------------------------------------------
+//Oranges are also available at the store. Add some code to your conditional so that it can handle the additional item. A purchase of less than 10 oranges costs 80 cents per orange. A purchase of between 11 and 20 oranges costs 60 cents per orange. Any purchase of more than 20 oranges costs a flat fee of $12.00"
+
+var item: String // = add either apple or orange here
+var pricePerOrange: Double // = 
+
+pricePerApple // = ___
+numberOfItems // = ___
+totalCost // = ___
+
+
+//write your new conditional statement here
+
+
+
+
+
+
+
+
+
+
+//print a message to the customer telling them the total cost of their purchase
+message // = ___
+print(message)
+
+
+
+
+
+
