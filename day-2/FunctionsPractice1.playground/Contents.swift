326827b68bc25c16d7c6bb97e347fb51c262d1de
jrectenwald <jimrec101@gmail.com>
2016-02-08 15:58:26 -0500

    function practice 1

---
+++ day-2/FunctionsPractice1.playground/Contents.swift
@@ -0,0 +1,92 @@
+import UIKit
+
+
+//Functions without return values
+var message = "Max greets people differently depending on his mood. Make four methods: happyGreeting, angryGreeting, confusedGreeting, and tiredGreeting. Make each of them print a greeting matching their name. Do not give it a return value."
+
+
+//define happyGreeting here
+
+
+
+
+
+
+//define angryGreeting here
+
+
+
+
+
+
+//define confusedGreeting here
+
+
+
+
+
+
+//define tiredGreeting here
+
+
+
+
+
+
+
+
+
+var mood: String = "" //Change the value of mood from happy, angry, confused, to tired.
+
+print("Hi Max, what's up?")
+
+//uncomment the function calls after you define them
+if mood == "happy" {
+    //happyGreeting()
+} else if mood == "angry" {
+    //angryGreeting()
+} else if mood == "confused" {
+    //confusedGreeting()
+} else if mood == "tired" {
+    //tiredGreeting()
+}
+
+
+//------------------------------------------------------------------------------------------------------
+//Functions with return values
+
+//make a function called myAge that returns the integer value of your age
+
+
+
+
+
+
+//make a function called fullName that returns a string containing your full name
+
+
+
+
+
+//make a function called homeTown that returns a string containing your hometown
+
+
+
+
+
+//uncomment this print statement once you finish the functions
+//print("Hi! My name is \(fullName()). I am \(myAge()) and I'm from \(homeTown()).")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
