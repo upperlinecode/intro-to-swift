af2f1b1a58716ce6e008751a0f3d17ed5eaeaaa5
jrectenwald <jimrec101@gmail.com>
2016-02-08 10:15:00 -0500

    rename array practice

---
+++ day-2/SubwayArrays.playground/Contents.swift
@@ -0,0 +1,58 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var message = ""
+
+var stations: [String] = ["14 St-Uniun Sq", "Astor Pl", "Bleeker St", "Spring St", "Canal St", "Brooklyn Bridge-City Hall", "Fulton St", "Wall St", "Bowling Green"]
+
+
+
+
+//Create a variable called numStops. Find the number of subway stations in the array and assign that integer to numStops
+
+
+
+
+//Now use some array methods to assign new values to the station variable
+var station: String = "One subway stop"
+
+
+
+//make station hold the name of the first stop in the array
+//station = someNewValue
+
+
+
+
+
+//now change the value to the last stop in the array
+
+
+
+
+
+//Last time - assign the fifth stop in the array to the station variable
+
+
+
+
+
+//The MTA is doing maintenance on the Astor Pl stop. Remove this index from the array.
+
+
+
+
+
+//The next stop after Bowling Green is Borough Hall. Add this to the last position in the array.
+
+
+
+
+
+//Somebody made a big mistake and misspelled Union Sq. Fix the spelling the array.
+
+
+
+
+//Use string interpolation to print out a message "Now arriving at Canal St. Transfer is available to the J, N, and Q trains. Next stop: Brooklyn Bridge-City Hall."
