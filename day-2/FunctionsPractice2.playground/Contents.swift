15f4c4d86ca0ec8e4295991dedfd4ee20ef96a7c
jrectenwald <jimrec101@gmail.com>
2016-02-08 18:46:28 -0500

    functions practice 2

--- day-2/FunctionsPractice2.playground/Contents.swift
+++ day-2/FunctionsPractice2.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var message = ""
+//Functions with parameters
+
+//Make a function called double that takes one integer parameter. Make it return twice the value of the parameter.
+//double(3) --> returns 6
+//double(20( --> returns 40
+
+
+
+
+
+
+
+//Make a function called repeat that takes in a string parameter and prints that string three times, seperated by spaces.
+//repeat("Banana") --> returns "Banana Banana Banana"
+//repeat("That kangaroo is Australian") --> returns "That kangaroo is Australian That kangaroo is Australian That kangaroo is Australian"
+
+
+
+
+
+
+//Make a function called pricePlusTax that calculates the price of a product plus 7% sales tax. It should accept a parameter of type Double
+//pricePlusTax(1.00) --> returns 1.07
+//pricePlusTax(7.15) --> returns 7.65
+
+
+
+
+
+
+
+//Define a function called
