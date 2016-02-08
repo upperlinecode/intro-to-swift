3af371d904d2e5b0e7f0f9a295f47a9e64a6b63d
jrectenwald <jimrec101@gmail.com>
2016-02-08 11:24:54 -0500

    add array methods practice

--- day-2/ArrayMethodsPractice.playground/Contents.swift
+++ day-2/ArrayMethodsPractice.playground/Contents.swift
@@ -2,15 +2,48 @@
 
 import UIKit
 
-var message = "Let's practice building some arrays! Make an array holding the names of the students at your table."
+var message = "Let's practice building some arrays! Make an array holding the first names of the students at your table."
 
-
-
-//Create the array here. Name the array 'students'
+//Create the array here. Name the array 'students1'.
 
 
 
 
 
+//Create another array to hold the names of the students at the table next to you. Call this 'students2'.
 
-//
+
+
+
+
+//Make up a name and insert it into the second position of students1
+
+
+
+
+
+//Make up another name and add it to the end of students2
+
+
+
+
+
+//Delete the last element of students1
+
+
+
+
+
+//Remove the third name in students2
+
+
+
+
+
+//Now combine the groups. Put all of the names in the students1 array.
+
+
+
+
+
+//Make a variable called numberOfStudents and store the total number of students in it.
