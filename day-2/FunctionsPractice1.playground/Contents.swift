c5f1aa000290c6f4dc8c1f3e2a37e84db314b302
James Rectenwald <jimrec101@gmail.com>
2016-02-08 23:12:29 -0500

    Update Contents.swift

--- day-2/FunctionsPractice1.playground/Contents.swift
+++ day-2/FunctionsPractice1.playground/Contents.swift
@@ -86,7 +86,7 @@ if mood == "happy" {
 
 
 
-
-
+//BONUS: Write a function that takes in a number of inches and returns the equivalent 
+//amount of centimeters (You'll have to look up how to use arguments...)
 
 
