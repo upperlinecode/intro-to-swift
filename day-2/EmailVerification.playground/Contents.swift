d92b3816574f4467382bd37d1a91f6ba089d0f27
jrectenwald <jimrec101@gmail.com>
2016-02-08 20:45:38 -0500

    add email verification lab

---
+++ day-2/EmailVerification.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Level 1
+//When users sign up for a website, it's common to have to register with an email address. You've probably noticed that a typo on one of these forms might result in an error message saying "Invalid Email Address".
+//Create a function called emailVerification that accepts a string parameter called 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------
+//Level 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------
+//Level 3
