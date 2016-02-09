bffabef86b59c2e0d45b8dc6d835d94db106c629
jrectenwald <jimrec101@gmail.com>
2016-02-08 19:02:40 -0500

    add five tasks for function practice 2

--- day-2/FunctionsPractice2.playground/Contents.swift
+++ day-2/FunctionsPractice2.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 //Make a function called double that takes one integer parameter. Make it return twice the value of the parameter.
 //double(3) --> returns 6
-//double(20( --> returns 40
+//double(20) --> returns 40
 
 
 
@@ -31,4 +31,28 @@ import UIKit
 
 
 
-//Define a function called
+//A person's max heart rate is approximately 220 minus their age. Define a function called maxHeartRate that takes in a parameter for the age of a person and returns the maximum heart rate for that person.
+//maxHeartRate(30) --> 190
+//maxHeartRate(17) --> 203
+
+
+
+
+
+
+//Make a function called todaysActivities that takes in a string parameter called weather. If it is raining, the function should return 'Staying inside and reading a book.'. If it is sunny, it will return 'Going swimming!'. If it is windy, it will return 'Flying a kite!' If weather contains any other string, it will return 'I don't know, I'll probably play it by ear.'
+//todaysActivities("windy") --> "Flying a kite."
+//todaysActivities("temperate") --> "I don't know, I'll probably play it by ear."
+
+
+
+
+
+
+
+
+
+
+
+
+
