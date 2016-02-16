e40ed290757279d8cf2237915d86f2a321f83356
jrectenwald <jimrec101@gmail.com>
2016-02-16 18:28:38 -0500

    edited final trivia project

--- day-3/TriviaAppDayThreeFinished/TriviaAppDayThreeStarter/ViewController.swift
+++ day-3/TriviaAppDayThreeFinished/TriviaAppDayThreeStarter/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Fact: UILabel!
+    let triviaModel = TriviaModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Fact.text = triviaModel.randomFact()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    
+    @IBAction func newFact() {
+        Fact.text = triviaModel.randomFact()
     }
 
-
 }
-
