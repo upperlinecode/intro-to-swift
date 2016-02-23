12576b3a2052434bd1ebf2b9240e78910bcfb6b3
jrectenwald <jimrec101@gmail.com>
2016-02-23 13:26:02 -0500

    adds oo practice 1

---
+++ day-4/OOPractice1.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//We just saw how we could represent a person using code. Now we're going to represent a dog the same way.
+
+class Dog {
+    
+    
+}
+
+
+
+//Work through the following tasks one by one, in order. Each time, add to the dog class above. Each task has sample output that you should be able to replicate when you are done.
+
+
+//1. 
+
+//2.
+
+//3.
+
+//4.
+
+//5.
+
+//6.
+
+//7.
+
+//8.
