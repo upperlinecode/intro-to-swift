01ad922196e1e00175a62ef68971e8464ed954d2
James Rectenwald <jimrec101@gmail.com>
2016-03-02 16:40:11 -0500

    Update Contents.swift

--- day-4/OOPractice2.playground/Contents.swift
+++ day-4/OOPractice2.playground/Contents.swift
@@ -36,6 +36,14 @@ struct Celsius {
         self.kelvin = self.celsius + 273.0
     }
     
+    func isBelowFreezing()->Bool {
+        if self.celsius < 0.0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
 }
 
 //var tenDegreesCelsius = Celsius(celsius: 10.0)
@@ -47,26 +55,12 @@ struct Celsius {
 
 
 
-//Bonus: Level Three
-//Create a struct called Temperature that has three properties: celsius, fahrenheit, and kelvin. Let the user only pass in a value for one of the three, and convert the other two values based on the argument
+//Bonus: Give the Celsius struct a method called isBelowFreezing that returns a boolean value of true if the temperature is below freezing.
 
 
 
-//var temp1 = Temperature(celsius: 10.0)
-//var temp2 = Temperature(fahrenheit: 50.0)
-//var temp3 = Temperature(kelvin: 283.0)
 
-//temp1 //returns 10.0
-//temp1 //returns 283.0
-//temp1 //returns 50.0
 
-//temp2 //returns 10.0
-//temp2 //returns 283.0
-//temp2 //returns 50.0
-
-//temp3 //returns 10.0
-//temp3 //returns 283.0
-//temp3 //returns 50.0
 
 
 
