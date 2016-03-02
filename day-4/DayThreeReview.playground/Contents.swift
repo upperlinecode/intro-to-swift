8419c0dd62e1426ec6f7cd6d5c98d51e7b450d15
jrectenwald <jimrec101@gmail.com>
2016-03-02 15:25:51 -0500

    add solution branch to day 3 review

--- day-4/DayThreeReview.playground/Contents.swift
+++ day-4/DayThreeReview.playground/Contents.swift
@@ -19,10 +19,36 @@ import UIKit
 //planetLocater("Mercury") //returns "Mercury is 57,910,000 km from the Sun."
 //planetLocater("Neptune") //returns "Neptune is 4,501,000,000 km from the Sun."
 
-func planetLocater(planet: String)->String {
-    return ""
+func planetLocater(planet: String = "Earth")->String {
+    var distance: Int
+    switch planet {
+    case "Mercury":
+        distance = 57910000
+    case "Venus":
+        distance = 108200000
+    case "Earth":
+        distance = 149500000
+    case "Mars":
+        distance = 227940000
+    case "Jupiter":
+        distance = 778330000
+    case "Saturn":
+        distance = 1424600000
+    case "Uranus":
+        distance = 2873550000
+    case "Neptune":
+        distance = 4501000000
+    case "Pluto":
+        distance = 5945900000
+    default:
+        distance = 149500000
+    }
+    
+    return "\(planet) is \(distance) km from the Sun."
 }
-
+planetLocater() //returns "Earth is 149,600,000 km from the Sun."
+planetLocater("Mercury") //returns "Mercury is 57,910,000 km from the Sun."
+planetLocater("Neptune") //returns "Neptune is 4,501,000,000 km from the Sun."
 
 
 //2. Write a method called fence that takes in an argument posts of type Int that prints out fences of differing lengths.
@@ -33,9 +59,15 @@ func planetLocater(planet: String)->String {
 //fence(15) //prints: |--|--|--|--|--|--|--|--|--|--|--|--|--|--|
 
 func fence(posts: Int) {
-    
+    var fence = "|"
+    for _ in 1...(posts - 1) {
+        fence += "-|"
+    }
+    print(fence)
 }
-
+fence(5) //prints: |--|--|--|--|
+fence(2) //prints: |--|
+fence(15) //prints: |--|--|--|--|--|--|--|--|--|--|--|--|--|--|
 
 
 //Bonus: A prime number is a number greater than 1 that can only be evenly divided by itself and 1. 
@@ -44,3 +76,36 @@ func fence(posts: Int) {
 //primes(1) //returns 2
 //primes(5) //returns 11
 //primes(21) //returns 73
+
+func primes(nthPrime: Int)->Int {
+    var primes: [Int] = [2]
+    var n = 3
+    while primes.count < nthPrime {
+        
+        //set the flag to true initially
+        var prime = true
+        
+        for var i = 2; i <= n - 1; i++ {
+                
+            //even division of a number thats not 1 or the number itself, not a prime number
+            if n % i == 0 {
+                prime = false
+                break
+            }
+        }
+            
+        if prime {
+            primes += [n]
+        }
+        
+        n += 1
+        prime = true
+    }
+    
+    let answer = primes[nthPrime - 1]
+    return answer
+}
+
+primes(1) //returns 2
+primes(5) //returns 11
+primes(21) //returns 73
