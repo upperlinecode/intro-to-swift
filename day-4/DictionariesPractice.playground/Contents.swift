70e97c9e5f4dc4b4051e070f0c91423be7cd20ca
James Rectenwald <jimrec101@gmail.com>
2016-03-02 15:55:12 -0500

    Update Contents.swift

--- day-4/DictionariesPractice.playground/Contents.swift
+++ day-4/DictionariesPractice.playground/Contents.swift
@@ -12,7 +12,7 @@ var distanceArray: [Double] = [6, 4, 5.5, 4.5]
 
 //1. Create a dictionary of type [String : Double] called runTracker where the keys are the dates of his workouts and the values are the distances of each workout.
 
-
+var runTracker = ["4/3/2015": 6, "4/5/2015": 4, "4/6/2015": 5.5, "4/7/2015": 4.5]
 
 
 
@@ -21,16 +21,33 @@ var distanceArray: [Double] = [6, 4, 5.5, 4.5]
 //displayWorkouts(runTracker)
 //prints:
 //   Date   |  Milage
-//---------------------
+//--------------------
 // 4/3/2015 |    6
 // 4/5/2015 |    4
 // 4/6/2015 |   5.5
 // 4/7/2015 |   4.5
 
-
-
+func displayWorkouts(workouts: [String: Double]) {
+    print("\tDate\t|\tMilage")
+    for (workoutDate, workoutMilage) in workouts {
+        print("\(workoutDate)\t|\t\(workoutMilage)")
+    }
+}
 
 //3. Add the following workout to the runTracker dictionary: date: 4/9/2015, distance: 7 miles. The run displayWorkouts again to make sure it was added correctly.
 
+runTracker["4/9/2015"] = 7
 
-//add remove and update
+
+//4. Remove the workout from 4/5/2015
+
+runTracker.removeValueForKey("4/5/2015")
+
+//5. Update the workout from 4/6/2015, giving it a distance of 6 miles
+
+runTracker["4/6/2015"] = 6
+
+
+
+
+displayWorkouts(runTracker)
