f20213773aa1a7dcf635c02feb82042261e9e4a4
James Rectenwald <jimrec101@gmail.com>
2016-03-02 16:40:57 -0500

    Update Contents.swift

--- day-4/OOPractice2.playground/Contents.swift
+++ day-4/OOPractice2.playground/Contents.swift
@@ -11,40 +11,14 @@ import UIKit
 //Make a struct called FreezingPoint that has three properties: celsius, fahrenheit, and kelvin. Give them all default values equal to the freezing point of water.
 
 
-struct FreezingPoint {
-    var celsius, fahrenheit, kelvin: Double
-    
-    init(celsius: Double = 0.0, fahrenheit: Double = 32.0, kelvin: Double = 273.0) {
-        self.celsius = celsius
-        self.fahrenheit = fahrenheit
-        self.kelvin = kelvin
-    }
-    
-}
-
 
 
 //Level Two
 //Make a struct called Celsius that has three properties: celsius, fahrenheit, and kelvin. Give celsius a default value of 0.0, and make the values of fahrenheit and kelvin correct values, converted from the celsius property.
 
-struct Celsius {
-    var celsius, fahrenheit, kelvin: Double
-    
-    init(celsius: Double = 0.0) {
-        self.celsius = celsius
-        self.fahrenheit = 1.8 * self.celsius + 32.0
-        self.kelvin = self.celsius + 273.0
-    }
-    
-    func isBelowFreezing()->Bool {
-        if self.celsius < 0.0 {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-}
+
+
+
 
 //var tenDegreesCelsius = Celsius(celsius: 10.0)
 
