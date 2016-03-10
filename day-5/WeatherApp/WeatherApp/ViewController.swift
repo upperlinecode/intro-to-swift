1f8a1cce73f9b6f715c803ca9825e671ed81ba7b
jrectenwald <jimrec101@gmail.com>
2016-03-10 16:23:52 -0500

    completed weather app, added screenshots

--- day-5/WeatherApp/WeatherApp/ViewController.swift
+++ day-5/WeatherApp/WeatherApp/ViewController.swift
@@ -10,18 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let weather = Weather()
+    var forcast: [String: [String:String]] = ["":["":""]]
+    
+    @IBOutlet weak var heading: UILabel!
+    @IBOutlet weak var headlineTemp: UILabel!
+    @IBOutlet weak var headlineDescription: UILabel!
+    
+    @IBOutlet weak var mondaySmallView: UILabel!
+    @IBOutlet weak var tuesdaySmallView: UILabel!
+    @IBOutlet weak var wednesdaySmallView: UILabel!
+    @IBOutlet weak var thursdaySmallView: UILabel!
+    @IBOutlet weak var fridaySmallView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        forcast = weather.newYorkForcast
+        updateLabels(forcast)
+        heading.text = "New York"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func newYorkWeather(sender: UIButton) {
+        forcast = weather.newYorkForcast
+        updateLabels(forcast)
+        heading.text = "New York"
     }
     
+    @IBAction func pittsburghWeather(sender: UIButton) {
+        forcast = weather.pittsburghForcast
+        updateLabels(forcast)
+        heading.text = "Pittsburgh"
+    }
+
+    @IBAction func bostonWeather(sender: UIButton) {
+        forcast = weather.bostonForcast
+        updateLabels(forcast)
+        heading.text = "Boston"
+    }
     
-
-
+    @IBAction func durhamWeather(sender: UIButton) {
+        forcast = weather.durhamForcast
+        updateLabels(forcast)
+        heading.text = "Durham"
+    }
+    
+    func updateLabels(forcast: [String: [String:String]]) {
+        headlineTemp.text = forcast["Monday"]!["temperature"]
+        headlineDescription.text = forcast["Monday"]!["description"]
+        
+        mondaySmallView.text = "Temperature: \(forcast["Monday"]!["temperature"]!)  Description: \(forcast["Monday"]!["description"]!)"
+        tuesdaySmallView.text = "Temperature: \(forcast["Tuesday"]!["temperature"]!)  Description: \(forcast["Tuesday"]!["description"]!)"
+        wednesdaySmallView.text = "Temperature: \(forcast["Wednesday"]!["temperature"]!)  Description: \(forcast["Wednesday"]!["description"]!)"
+        thursdaySmallView.text = "Temperature: \(forcast["Thursday"]!["temperature"]!)  Description: \(forcast["Thursday"]!["description"]!)"
+        fridaySmallView.text = "Temperature: \(forcast["Friday"]!["temperature"]!)  Description: \(forcast["Friday"]!["description"]!)"
+    }
 }
 
