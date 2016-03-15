68a8b6eff75588aa712ecdc922a241c300cc214e
jrectenwald <jimrec101@gmail.com>
2016-03-15 13:02:52 -0400

    add solution

--- day-5/ObjectOrientedLabels/ObjectOrientedLabels/ViewController.swift
+++ day-5/ObjectOrientedLabels/ObjectOrientedLabels/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
- 
+    var labelGenerator: CustomLabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        labelGenerator = CustomLabel(view: self.view)
+    }
+    
+    
+    @IBAction func success(sender: UIButton) {
+        if labelGenerator?.success() != nil {
+            print("label generated")
+        } else {
+            print("Error: label generator is nil")
+        }
+    }
+    
+    @IBAction func warning(sender: UIButton) {
+        if labelGenerator?.warning() != nil {
+            print("label generated")
+        } else {
+            print("Error: label generator is nil")
+        }
+    }
+    
+    @IBAction func info(sender: UIButton) {
+        if labelGenerator?.info() != nil {
+            print("label generated")
+        } else {
+            print("Error: label generator is nil")
+        }
+    }
 }
 
