96883588e4ecde9616dd4a7c02f3ef459a212fbd
James Rectenwald <jimrec101@gmail.com>
2016-03-15 14:33:29 -0400

    Update Contents.swift

--- day-1/CalculatorLab.playground/Contents.swift
+++ day-1/CalculatorLab.playground/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 import Darwin
 
-var mathProblem = "Welcome to Upperline Calculator! Follow the directions in the comments and fill in the missing values to complete the calculations. The 'user' will input two numbers into the variables num1 and num2, and you will complete the appropriate operation to the right of the answer variable."
+var mathProblem = "Welcome to Upperline Calculator! Follow the directions in the comments and fill in the missing values to complete the calculations"
 
 
 //-------------------------------------------------------------------------------------------------
@@ -16,8 +16,8 @@ var num2 = 1
 
 var answer = num1 + num2
 
-// interpolate a string inside the print statement so that it prints the string "1 + 1 = 2"
-print("")
+//interpolate so that it prints the string "1 + 1 = 2"
+print("\(num1) + \(num2) = \(answer)")
 
 //-------------------------------------------------------------------------------------------------
 // 1
@@ -26,10 +26,10 @@ num1 = 55
 
 num2 = 40
 
-answer // caculate the new subtraction problem and store the answer in this variable
+answer = 55 - 40
 
-// interpolate so that it prints the string "55 - 40 = 15"
-print("")
+//interpolate so that it prints the string "55 - 40 = 15"
+print("\(num1) - \(num2) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
@@ -39,68 +39,68 @@ num1 = 4
 
 num2 = 3
 
-answer
+answer = num1 * num2
 
-// interpolate so that it prints the string "4 * 3 = 12"
-print("")
+//interpolate so that it prints the string "4 * 3 = 12"
+print("\(num1) x \(num2) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
 // 3
 // division
-num1
+num1 = 20
 
-num2
+num2 = 4
 
-answer
+answer = 20 / 4
 
-// interpolate so that it prints the string "20 / 4 = 5"
-print("")
+//interpolate so that it prints the string "20 / 4 = 5"
+print("\(num1) / \(num2) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
 // 4
 // modulus
 // use the modulus (%) operator to find the remainder when you divide 453 / 7
-num1
+num1 = 453
 
-num2
+num2 = 7
 
-answer
+answer = 453 % 7
 
-// interpolate so that it prints the string, complete with the answer "453 % 7 = ___"
-print("")
+//interpolate so that it prints the string, complete with the answer "453 % 7 = ___"
+print("\(num1) % \(num2) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
 // 5
 // exponents
 // use a method in the Darwin library to find the answer to 54^3
-num1
+num1 = 54
 
-num2
+num2 = 3
 
-answer
+var power = pow(54.0, 3.0)
 
-// interpolate so that it prints the string "54^3 = ___"
-print("")
+//interpolate so that it prints the string "54^3 = ___"
+print("\(num1)^\(num2) = \(power)")
 
 
 //-------------------------------------------------------------------------------------------------
 // 6
-// combining multiple operators
-num1
+// mixed bag
+num1 = 60
 
-num2
+num2 = 5
 
-var num3: Int
+var num3: Int = 8
 
-var num4: Int
+var num4: Int = 3
 
-answer
+answer = (num1 / num2) + (num3 % num4)
 
-// interpolate so that it prints the string "(60 / 5) + (8 % 3)"
-print("")
+//interpolate so that it prints the string "(60 / 5) + (8 % 3)"
+print("(\(num1) / \(num2)) + (\(num3) % \(num4)) = \(answer)")
 
 
 //-------------------------------------------------------------------------------------------------
@@ -108,18 +108,8 @@ print("")
 // mathematical constants
 // use the Darwin library to get the value of pi
 
-// var pi = ___
-
-answer
-
-// interpolate so that it prints the string "The value of pi is approximately 3.141592653589793"
-print("")
+var pi = M_PI
 
 
-//-------------------------------------------------------------------------------------------------
-// 8 
-// Imani spent half of her weekly allowance playing mini-golf. To earn more money her parents let her wash the car for $4. What is her weekly allowance if she ended with $12?
-
-
-
-
+//interpolate so that it prints the string "The value of pi is approximately 3.141592653589793"
+print(pi)
