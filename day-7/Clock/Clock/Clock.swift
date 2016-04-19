d99e3cb82411c3f449e92f699f95d485a22a9003
jrectenwald <jimrec101@gmail.com>
2016-04-18 21:17:11 -0400

    add clock project

---
+++ day-7/Clock/Clock/Clock.swift
@@ -0,0 +1,16 @@
+//
+//  Clock.swift
+//  Clock
+//
+//  Created by flatironstudent on 4/17/16.
+//  Copyright © 2016 flatironstudent. All rights reserved.
+//
+
+import Foundation
+
+class Clock {
+    
+    func getCurrentTime() -> NSDate {
+        return NSDate()
+    }
+}
