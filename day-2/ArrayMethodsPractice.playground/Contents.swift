f3e85ebdc1ac7e930ac3dd2c96d9ae55b9c0c87f
Jeff <jolson615@gmail.com>
2018-06-19 08:54:28 -0400

    Update Contents.swift

--- day-2/ArrayMethodsPractice.playground/Contents.swift
+++ day-2/ArrayMethodsPractice.playground/Contents.swift
@@ -1,7 +1,5 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
-
 var message = "Let's practice building some arrays! Make an array holding the first names of the students at your table."
 
 //Create the array here. Name the array 'students1'.
